a148eea8a5836c2d36959e2b1a973ad23a586acf
harry.akbar <harryakbaram@gmail.com>
2017-12-21 22:32:01 +0700

    Change host, flask version

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def application_error(e):
     return 'Sorry, unexpected error: {}'.format(e), 500
 
 if __name__ == '__main__':
-    app.run(debug = True, port=5000)
+    app.run(host="0.0.0.0",debug=True, port=5000)
